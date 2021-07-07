7ac2c3bb2ed05324a4cb723145dfc22110813c83
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-07-07 19:44:18 +0530

    # adding controller class

---
+++ rest-api-validation/src/main/java/com/ganesh/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.ganesh.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ganesh.model.User;
+import com.ganesh.service.UserService;
+
+@RestController
+public class UserController {
+
+    Logger logger = LoggerFactory.getLogger("UserManagementApp");
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        logger.info("getting users..");
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable int id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public User deleteUser(@PathVariable int id) {
+        return userService.deleteUser(id);
+    }
+
+}
+
--- rest-api-validation/src/main/java/com/ganesh/service/impl/UserServiceImpl.java
+++ rest-api-validation/src/main/java/com/ganesh/service/impl/UserServiceImpl.java
@@ -1,5 +1,43 @@
 package com.ganesh.service.impl;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ganesh.model.User;
+import com.ganesh.repository.UserRepository;
+import com.ganesh.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public List<User> getAllUsers() {
+		return userRepository.getAllUsers();
+	}
+
+	@Override
+	public User getUser(int id) {
+		return userRepository.getUser(id);
+	}
+
+	@Override
+	public User addUser(User user) {
+		return userRepository.addUser(user);
+	}
+
+	@Override
+	public User updateUser(User user) {
+		return userRepository.updateUser(user);
+	}
+
+	@Override
+	public User deleteUser(int id) {
+		return userRepository.deleteUser(id);
+	}
 
 }
