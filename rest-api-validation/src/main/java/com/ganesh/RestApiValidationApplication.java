9fbe05609f49c13aabdd1af421a698c17668abf8
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-07-07 19:21:43 +0530

    ## adding new project api-validation

---
+++ rest-api-validation/src/main/java/com/ganesh/RestApiValidationApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiValidationApplication.class, args);
+	}
+
+}
