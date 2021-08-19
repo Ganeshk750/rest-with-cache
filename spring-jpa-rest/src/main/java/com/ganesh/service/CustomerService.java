c3c95f530c92f8441219e40d13484170d6dec69c
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-08-19 17:57:45 +0530

    # Code

---
+++ spring-jpa-rest/src/main/java/com/ganesh/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.ganesh.service;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+	
+	
+
+}
